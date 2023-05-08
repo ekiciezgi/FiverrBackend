804923478ecac3846fde7497623855a4b837282f
Ezgi Ekici <ekiciezgi3@icloud.com>
2023-05-08 23:48:18 +0300

    push 8.05

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -48,5 +48,9 @@ export const login = async (req, res, next) => {
     }
 };
 export const logout = async (req, res) => {
-    res.send("from controller")
+    res.clearCookie("accessToken", {
+        sameSite: "none",
+        secure: true,
+    }).status(200)
+        .send("User has been logged out.")
 }
--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import authRoute from "./routes/auth.route.js";
 import userRoute from "./routes/user.route.js";
 import cookieParser from "cookie-parser";
+import cors from "cors"
 // import gigRoute from "./routes/gig.route.js";
 // import orderRoute from "./routes/order.route.js";
 // import conversationRoute from "./routes/conversation.route.js";
@@ -22,6 +23,7 @@ const connected = async () => {
         console.log(error)
     }
 }
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser())
 app.use("/api/auth", authRoute);
